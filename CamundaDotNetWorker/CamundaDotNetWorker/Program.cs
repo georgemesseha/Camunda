faa8aaffa77364f86875b4d6bc473c99046e2839
george.saeed <george.saeed@softec.ai>
2025-08-26 15:07:29 +0300

    Just before changing ports

--- CamundaDotNetWorker/CamundaDotNetWorker/Program.cs
+++ CamundaDotNetWorker/CamundaDotNetWorker/Program.cs
@@ -27,13 +27,26 @@ public class Program
                       .UseContactPoint(ContactPoint)
                       .Build();
         
+        _ = ReceiveInput();
+        
         // Starting the Job Worker
         using (var signal = new EventWaitHandle(false, EventResetMode.AutoReset))
         {
-	        string jobType = "dotnet-work";
+	        string jobType1 = "dotnet-work" + 1;
 	        zeebeClient.NewWorker()
-	                   .JobType(jobType)
-	                   .Handler(TriggerApproximation)
+	                   .JobType(jobType1)
+	                   .Handler(WorkerHandler_dotnet_work1)
+	                   .MaxJobsActive(5)
+	                   .Name(Environment.MachineName)
+	                   .AutoCompletion()
+	                   .PollInterval(TimeSpan.FromSeconds(1))
+	                   .Timeout(TimeSpan.FromSeconds(10))
+	                   .Open();
+	        
+	        string jobType2 = "dotnet-work" + 2;
+	        zeebeClient.NewWorker()
+	                   .JobType(jobType2)
+	                   .Handler(WorkerHandler_dotnet_work2)
 	                   .MaxJobsActive(5)
 	                   .Name(Environment.MachineName)
 	                   .AutoCompletion()
@@ -43,13 +56,61 @@ public class Program
 
 	        signal.WaitOne();
         }
+        
+       
+    }
+
+    private static async Task ReceiveInput()
+    {
+	    await Task.Run(async () =>
+	             {
+		             while (true)
+		             {
+			             Console.WriteLine("Write a message to start the process:");
+			             string? processStartMessage = Console.ReadLine();
+			             if (processStartMessage != null)
+			             {
+				             await StartNewWorkflow();
+			             }
+		             }
+	             });
+
     }
     
-    private static void TriggerApproximation(IJobClient jobClient, IJob job)
+    private static async Task StartNewWorkflow()
+    {
+	    var payload = new {
+		    orderId = 123,
+		    userId = "abc"
+	    };
+	    var jPayload = JsonConvert.SerializeObject(payload);
+	        
+	    await zeebeClient.NewPublishMessageCommand()
+	                     .MessageName("start-workflow")          // Must match BPMN message name
+	                     .CorrelationKey("business-key-or-similar") // Matches BPMN `Message Catch Event`
+	                     .Variables(jPayload)
+	                     .Send();
+    }
+    
+    private static void WorkerHandler_dotnet_work1(IJobClient jobClient, IJob job)
     {
 	    JObject jsonObject = JObject.Parse(job.Variables);
 	    
-	    Console.WriteLine("Working on Task");
+	    Console.WriteLine("Working on Task"); 
+	  
+	    jobClient.NewCompleteJobCommand(job.Key)
+	             .Variables("""{"status": "Completed", "person":{"name": "george", "age": 25}}""")
+	             .Send()
+	             .GetAwaiter()
+	             .GetResult();
+	    Console.WriteLine("Completed the fetched Task");
+    }  
+    
+    private static void WorkerHandler_dotnet_work2(IJobClient jobClient, IJob job)
+    {
+	    JObject jsonObject = JObject.Parse(job.Variables);
+	    
+	    Console.WriteLine("Working on Task"); 
 	  
 	    jobClient.NewCompleteJobCommand(job.Key)
 	             .Variables("""{"status": "Completed", "person":{"name": "george", "age": 25}}""")
--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # Camunda 8 Self-Managed - Docker Compose
 
+#To get started
+Just run docker compose up and expect the following interface
+
+#Main tools
+##Web Modeler
+http://localhost:8070
+
+##Tasklist
+http://localhost:8082/tasklist
+
+##Operate
+http://localhost:8081/operate
+
+
 ## Usage
 
 For end user usage, please check the offical documentation of [Camunda 8 Self-Managed Docker Compose](https://docs.camunda.io/docs/next/self-managed/setup/deploy/local/docker-compose/).
